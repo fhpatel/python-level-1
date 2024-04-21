363062e1c59e151e9fd47f955b8f4f0732dc7150
Faisal Patel <faisalhussien@hotmail.com>
2024-04-21 23:39:20 +0100

    Add files via upload

---
+++ LIFT_week_1.py
@@ -0,0 +1,11 @@
+#SIMPLE CALCULATOR PROJECT
+
+print("Welcome to the simple calculator, this program will multiply two numbers together")
+
+first_number = int(input("Please input the first number: "))
+
+second_number = int(input("Please input your second number: "))
+
+total = first_number * second_number
+
+print(f"The product of {first_number} and {second_number} is {total}")
